c0c17cd0078b95e6d28bf6bc463b86b76f2fa009
ArielWangX <arielxiewang@gmail.com>
2023-02-15 15:59:48 +1100

    Build and Modify an Array

--- 07_04/script.js
+++ 07_04/script.js
@@ -9,3 +9,24 @@
  */
 
 
+// Build an array with 8 items
+const array = ["pen", 5, true, 3.2, "backpack", "apple", "car", "flower"];
+console.log(array);
+
+// Remove the last item
+array.pop();
+
+// Add the last item as the first item on the array 
+array.unshift("car");
+
+// Sort the items by alphabetical order
+array.sort();
+
+//Use the find() method to find a specific item in the array
+const foundItem = array.find((item) => item === "car");
+console.log(foundItem);
+
+// Remove the item you found using the find method from the array.
+array.splice(array.indexOf(foundItem), 1)
+
+console.log(array);
